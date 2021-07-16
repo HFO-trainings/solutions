b00f45b59334226da15da3275a60bdc5f4f7ee3d
computerbox <computerbox124@gmail.com>
2021-07-16 21:17:22 +0400

    * Added solution of problem D

---
+++ HFO Speed Contest #1/d.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int n;
+vector<int>massiv;
+
+int main(){
+ios_base::sync_with_stdio(0);
+cin >> n;
+for(int i = 1; i <= n; i++)
+{
+  int x;
+  cin >> x;
+  massiv.push_back(x);
+}
+
+sort(massiv.begin(),massiv.end());
+int raz = 0;
+for(int i = 0; i < n - 1; i++)
+{
+  if(massiv[i + 1] - massiv[i] > 0){raz = massiv[i + 1] - massiv[i];break;}
+}
+for(int i = 0; i < n - 1; i++)
+{
+  if(massiv[i + 1] - massiv[i] > 0)raz = __gcd(raz,massiv[i + 1] - massiv[i]);
+}
+ll ans = 0;
+for(int i = 0; i < n - 1; i++)
+{
+ ll vz = massiv[n - 1] - massiv[i];
+ ans += vz / raz;
+}
+
+cout << ans << " " << raz << endl;
+return 0;
+}
