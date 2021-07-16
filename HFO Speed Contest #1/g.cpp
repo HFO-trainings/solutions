e8832ffa65283693cc48268845421d133f4f9778
computerbox <computerbox124@gmail.com>
2021-07-16 21:10:45 +0400

    * Added solution of problem G

---
+++ HFO Speed Contest #1/g.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+
+
+using namespace std;
+
+int n;
+
+vector<pair<int,int> >massiv;
+set<int>points;
+
+int main(){
+ios_base::sync_with_stdio(0);
+cin >> n;
+for(int i = 1; i <= n; i++)
+{
+  int x, y;
+  cin >> x >> y;
+  massiv.push_back(make_pair(x,y));
+  points.insert(x);
+}
+
+sort(massiv.begin(),massiv.end());
+//Adding not important points for comfortable binary search
+points.insert(-INT_MAX);
+points.insert(INT_MAX);
+
+int r = -INT_MAX;
+int ans = 0;
+for(int i = 0; i < n; i++)
+{
+  int x = massiv[i].first;
+  int y = massiv[i].second;
+  ans++;
+  if(x - y > r && *(--points.lower_bound(x)) < x - y)r = x;// left side
+  else if(x + y > r && (*points.upper_bound(x)) > (x + y))r = x + y; //right side
+  else ans--;//center
+}
+
+cout << ans << endl;
+
+return 0;
+}
